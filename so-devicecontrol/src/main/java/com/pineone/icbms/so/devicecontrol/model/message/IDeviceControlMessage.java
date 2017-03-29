d1567498f6de9a72fe1f8cec2f875f4f838018c7
greeun <github.com@greeun.com>
2017-03-29 17:21:33 +0900

    build env values modified.

--- so-devicecontrol/src/main/java/com/pineone/icbms/so/devicecontrol/model/message/IDeviceControlMessage.java
+++ so-devicecontrol/src/main/java/com/pineone/icbms/so/devicecontrol/model/message/IDeviceControlMessage.java
@@ -2,6 +2,7 @@ package com.pineone.icbms.so.devicecontrol.model.message;
 
 /**
  * DeviceControl Message interface.<BR/>
+ *
  * Created by uni4love on 2017. 1. 20..
  */
 public interface IDeviceControlMessage {
--- test/src/main/java/com/pineone/icbms/so/test/ObjectModelMapperForMQ.java
+++ test/src/main/java/com/pineone/icbms/so/test/ObjectModelMapperForMQ.java
@@ -1,13 +1,5 @@
 package com.pineone.icbms.so.test;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.pineone.icbms.so.processor.context.messagequeue.model.ContextInformationForMQ;
-import com.pineone.icbms.so.processor.context.messagequeue.model.ContextModelForMQ;
-import com.pineone.icbms.so.util.id.IdUtils;
-
-
 /**
  * object mapper test.<BR/>
  *
@@ -18,25 +10,25 @@ public class ObjectModelMapperForMQ {
      * test context model.
      */
     private void testContextModelSerialize() {
-        //context model
-        ContextModelForMQ cm = new ContextModelForMQ(IdUtils.createRandomUUID(), "test ContextModelForMQ");
-
-        int contextInformationCount = 2;
-        //context information
-        ContextInformationForMQ ci = null;
-        for(int i=0; i<contextInformationCount; i++) {
-            ci = new ContextInformationForMQ(IdUtils.createRandomUUID(), "test ContextInformation " + i);
-            cm.addContextInformation(ci);
-        }
-        //obj mapper
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-        try {
-            String cmString = objectMapper.writeValueAsString(cm);
-            System.out.println(cmString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+//        //context model
+//        ContextModelForMQ cm = new ContextModelForMQ(IdUtils.createRandomUUID(), "test ContextModelForMQ");
+//
+//        int contextInformationCount = 2;
+//        //context information
+//        ContextInformationForMQ ci = null;
+//        for(int i=0; i<contextInformationCount; i++) {
+//            ci = new ContextInformationForMQ(IdUtils.createRandomUUID(), "test ContextInformation " + i);
+//            cm.addContextInformation(ci);
+//        }
+//        //obj mapper
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+//        try {
+//            String cmString = objectMapper.writeValueAsString(cm);
+//            System.out.println(cmString);
+//        } catch (JsonProcessingException e) {
+//            e.printStackTrace();
+//        }
     }
 
     /**
